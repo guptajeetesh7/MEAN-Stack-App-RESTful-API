f36021788d646e6a0ff5bc7cb49a625caae2e476
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-08-01 21:58:22 +0530

    Setting Up Angular Js

---
+++ public/controllers/main.js
@@ -0,0 +1,6 @@
+var app = angular.module('MEAN',[]);
+
+app.controller('main',function($scope){
+
+	console.log('Running');
+});
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
+
+//setting up the express js
 var app = express();
 
-app.get('/',function(req,res){
-	res.send('Hello world from server.js');
-});
+//use the static files
+app.use(express.static(__dirname + '/public'));
+
+
 
 app.listen(3000);
 console.log('server running');
