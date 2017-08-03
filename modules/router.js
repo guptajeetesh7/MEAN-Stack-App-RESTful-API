0292cf84eb234d9bde3fe32f3d3df8a92c68a646
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-08-03 13:51:02 +0530

    MongoDB connected via Mongoose

--- modules/router.js
+++ modules/router.js
@@ -1,4 +1,24 @@
+//Import mongoose library to use MongoDB
+var mongoose = require('mongoose');
 
+//change the Promise to global => ES6 Promise
+mongoose.Promise = global.Promise;
+
+
+//connect to MongoDb Locally::
+mongoose.connect('mongodb://localhost/REST',{ useMongoClient: true });
+
+// '/REST' is the database. => REST is database is the name of the database.
+
+//checking the connection to the database
+mongoose.connection.once('open',function(){
+	console.log('Connection Has been made Succesfully!!');
+}).on('error',function(error){
+	console.log('Error to the connection :' + error);
+});
+
+
+//Exporting the module
 module.exports = function(app){
 
 		//IF get request is requested
